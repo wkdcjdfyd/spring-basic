e2d8b1033bea2d1e1f78e3178bceebc2b9eb43d0
장청룡 <wkdcjdfyd@naver.com>
2023-10-09 17:14:52 +0900

    Create RateDiscountPolicy

---
+++ core/src/test/java/com/ryong/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,39 @@
+package com.ryong.core.discount;
+
+import com.ryong.core.member.Grade;
+import com.ryong.core.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용돼야 한다.")
+    void vip_o(){
+        // given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+        // when
+        int discount = discountPolicy.discount(member, 10000);
+
+        // then
+        assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다.")
+    void vip_x(){
+        // given
+        Member member = new Member(1L, "memberBASIC", Grade.BASIC);
+
+        // when
+        int discount = discountPolicy.discount(member, 10000);
+
+        // then
+        assertThat(discount).isEqualTo(0);
+    }
+}
