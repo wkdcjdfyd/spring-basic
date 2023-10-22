a715c5f7fa961c6bf94315dea81a8e6181d15733
장청룡 <wkdcjdfyd@naver.com>
2023-10-22 23:34:51 +0900

    Create ComponentFilterAppConfigTest

---
+++ core/src/test/java/com/ryong/core/scan/filter/BeanB.java
@@ -0,0 +1,6 @@
+package com.ryong.core.scan.filter;
+
+
+@MyExcludeComponent
+public class BeanB {
+}
---
+++ core/src/test/java/com/ryong/core/scan/filter/MyIncludeComponent.java
@@ -0,0 +1,10 @@
+package com.ryong.core.scan.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyIncludeComponent {
+
+}
